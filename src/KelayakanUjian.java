b587f5438fa2062039ad3301052e5ec6f7efcb53
roy-2582026 <2582026@unai.edu>
2026-09-22 20:16:35 +0700

    nilai tugas

--- src/KelayakanUjian.java
+++ src/KelayakanUjian.java
@@ -8,6 +8,8 @@ public class KelayakanUjian {
 
         System.out.print("Kehadiran (%) : ");
         int kehadiran = input.nextInt();
+        System.out.print("Nilai tugas : ");
+        int nilaiTugas = input.nextInt();
 
         boolean dispensasi = input.nextBoolean();
 
@@ -27,8 +29,7 @@ public class KelayakanUjian {
 
         System.out.println();
         System.out.println(" KELAYAKAN UJIAN ");
-
-
+        System.out.println("Nilai tugas: " + nilaiTugas);
 
         System.out.println();
         System.out.println("a (tanpa kurung) : " + a);
