a1f3f81a2a79fe61710585043da1df0b167a0980
roy-2582026 <2582026@unai.edu>
2026-09-22 19:57:14 +0700

    Tugas Kelayakan Ujian

---
+++ src/KelayakanUjian.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class KelayakanUjian {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kehadiran (%) : ");
+        int kehadiran = input.nextInt();
+
+        System.out.print("Nilai tugas : ");
+        int nilaiTugas = input.nextInt();
+
+        System.out.print("Dispensasi (true/false) : ");
+        boolean dispensasi = input.nextBoolean();
+
+        boolean a = kehadiran >= 75 && nilaiTugas >= 60 || dispensasi;
+        boolean b = (kehadiran >= 75 && nilaiTugas >= 60) || dispensasi;
+        boolean c = kehadiran >= 75 && (nilaiTugas >= 60 || dispensasi);
+
+        boolean negasiDispensasi = !dispensasi;
+
+        int cek = 0;
+
+        boolean x = (kehadiran >= 75) && (cek++ > 0);
+        boolean y = (nilaiTugas >= 60) || (cek++ > 0);
+
+        System.out.println();
+        System.out.println("===== KELAYAKAN UJIAN =====");
+        System.out.println("Kehadiran: " + kehadiran + "%");
+        System.out.println("Nilai tugas: " + nilaiTugas);
+        System.out.println("Dispensasi : " + dispensasi);
+
+        System.out.println();
+        System.out.println("a (tanpa kurung) : " + a);
+        System.out.println("b (prioritas Kurung): " + b);
+        System.out.println("c (kurung digeser) : " + c);
+        System.out.println("!dispensasi : " + negasiDispensasi);
+        System.out.println("cek dipanggil : " + cek);
+
+        input.close();
+    }
+}
